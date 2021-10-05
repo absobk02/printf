2cf639129a3e304c51fd230df78e6295433e08c3
Abel <absobk02@gmail.com>
2021-10-05 12:08:09 -0700

    print binary

--- print_binary.c
+++ print_binary.c
@@ -6,32 +6,15 @@
  *
  * Return: no of resulting binary characters
  */
-int print_binary(unsigned int num)
-{
-	int chars_printed = 0, idx = 0;
-	char bin[sizeof(unsigned int) * 8];
 
-	if (num == 0)
-	{
-		chars_printed += _putchar('0');
-		return (chars_printed);
-	}
-	if (num == 1)
-	{
-		chars_printed += _putchar('1');
-		return (chars_printed);
-	}
-	while (num > 0)
-	{
-		bin[idx] = '0' + (num % 2);
-		num = num / 2;
-		idx++;
-	}
-	idx--;
-	while (idx >= 0)
-	{
-		chars_printed += _putchar(bin[idx]);
-		idx--;
-	}
-	return (chars_printed);
+int print_binary(va_list list)
+{
+	char *p_buff;
+	int size;
+
+	p_buff = itoa(va_arg(list, unsigned int), 2);
+
+	size = print(p_buff);
+
+	return (size);
 }
