2f5e99c0d5b2992b2dd788078f952d0717e2afc1
Abel <absobk02@gmail.com>
2021-10-08 04:54:38 -0700

    Print function

--- main.h
+++ main.h
@@ -1,17 +1,17 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
-
 #include <stdarg.h>
 /**
- * struct print - structure for printing various types
- * @t: type to print
- * @f: function to print
+ *struct print - structure for printing various types 
+ *@t: type to print 
+ *@f: function to print
  */
 typedef struct print
 {
-	char *t;
-	int (*f)(va_list);
-} print_t;
+char *t;
+int (*f)(va_list);
+}
+print_t;
 
 int _putchar(char c);
 int _printf(const char *format, ...);
